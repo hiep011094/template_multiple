bc26a9ec67bd48936af4eab5c04ffe89a5307b3b
hiep011094 <hiep011094@gmail.com>
2024-12-13 14:16:32 +0700

    hiep

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,133 +3,233 @@ import plumber from 'gulp-plumber';
 import ejs from 'gulp-ejs';
 import frontMatter from 'gulp-front-matter';
 import wrapper from 'layout-wrapper';
-import rename from "gulp-rename";
+import rename from 'gulp-rename';
 var cache = require('gulp-cached');
 
-import browser from "browser-sync";
+import browser from 'browser-sync';
 const browserSync = browser.create();
-import autoprefixer from "gulp-autoprefixer";
+import autoprefixer from 'gulp-autoprefixer';
 const sass = require('gulp-sass')(require('sass'));
-import sourcemaps from "gulp-sourcemaps";
-import sortMediaQueries from "postcss-sort-media-queries";
-import postcss from "gulp-postcss";
-import del from "del";
-
+import sourcemaps from 'gulp-sourcemaps';
+import sortMediaQueries from 'postcss-sort-media-queries';
+import postcss from 'gulp-postcss';
+import del from 'del';
 
 const path = {
-  ejs: {
-    layoutDir: `${__dirname}/src/layouts`,
-    src: [
-      './src/**/*.ejs',
-      '!./src/**/_*.ejs'
-    ],
-    dist: './dits/'
-  },
-  json: {
-    package: './package.json',
-    newsList: './src/_data/newsList.json'
-  }
+    ejs: {
+        layoutDir: `${__dirname}/src/layouts`,
+        src: ['./src/**/*.ejs', '!./src/**/_*.ejs'],
+        dist: './dits/',
+    },
+    json: {
+        package: './package.json',
+        newsList: './src/_data/newsList.json',
+    },
 };
 
+const fs = require("fs");
+
+const filePath = "build_xampp.txt";
+let path_xampp = "";
+if (fs.existsSync(filePath)) {
+    path_xampp = fs.readFileSync(filePath, "utf8");
+}
 
 global.jsonData = {};
 jsonData.newsListJson = require(path.json.newsList);
 
 // Clean assets
 function clean() {
-    return del(["./dits/assets/"]);
+    return del(['./dits/assets/']);
 }
 
 export function style() {
-   
-    return gulp
+    if(path_xampp){
+        return gulp
         .src('./src/assets/scss/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(
+            sass({
+                outputStyle: 'expanded',
+            }).on('error', sass.logError)
+        )
         .pipe(sourcemaps.write('.'))
         .pipe(cache('linting'))
         .pipe(gulp.dest('./dits/assets/css'))
+        .pipe(gulp.dest(path_xampp + '/assets/css'));
+    }else{
+        return gulp
+        .src('./src/assets/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                outputStyle: 'expanded',
+            }).on('error', sass.logError)
+        )
+        .pipe(sourcemaps.write('.'))
+        .pipe(cache('linting'))
+        .pipe(gulp.dest('./dits/assets/css'));
+    }
+    
 }
 
 //JS
-export function scripts(){
-    return gulp
+export function scripts() {
+    if(path_xampp){
+        return gulp
+        .src('./src/assets/js/**/*.js')
+        .pipe(gulp.dest('./dits/assets/js'))
+        .pipe(gulp.dest(path_xampp + '/assets/js'));
+    }else{
+        return gulp
         .src('./src/assets/js/**/*.js')
         .pipe(gulp.dest('./dits/assets/js'));
+    }
+    
 }
 
-export function css(){
-  return gulp
-      .src('./src/assets/css/**/*.css')
-      .pipe(gulp.dest('./dits/assets/css'));
+export function css() {
+    if(path_xampp){
+        return gulp
+        .src('./src/assets/css/**/*.css')
+        .pipe(gulp.dest('./dits/assets/css'))
+        .pipe(gulp.dest(path_xampp + '/assets/css'));
+    }else{
+        return gulp
+        .src('./src/assets/css/**/*.css')
+        .pipe(gulp.dest('./dits/assets/css'));
+    }
+    
 }
 
 //Images
-export function images(){  
-  return gulp
+export function images() {
+    if(path_xampp){
+        return gulp
         .src('./src/assets/images/**/*.+(jpg|jpeg|png|gif|webp|svg|ico)')
-        .pipe(gulp.dest("./dits/assets/images"));
+        .pipe(gulp.dest('./dits/assets/images'))
+        .pipe(gulp.dest(path_xampp + '/assets/images'));
+    }else{
+        return gulp
+        .src('./src/assets/images/**/*.+(jpg|jpeg|png|gif|webp|svg|ico)')
+        .pipe(gulp.dest('./dits/assets/images'));
+    }
+    
 }
 
+//videos
+export function videos() {
+    if(path_xampp){
+        return gulp
+        .src('./src/assets/videos/**/*.+(mp4|webm|ogg)')
+        .pipe(gulp.dest('./dits/assets/videos'))
+        .pipe(gulp.dest(path_xampp + '/assets/videos'));
+    }else{
+        return gulp
+        .src('./src/assets/videos/**/*.+(mp4|webm|ogg)')
+        .pipe(gulp.dest('./dits/assets/videos'));
+    }
+    
+}
+
+//videos
+export function php_wp() {
+    if(path_xampp){
+        return gulp
+        .src('./src/php_wp/**/*.+(php|png|jpg|css|js)')
+        .pipe(gulp.dest('./dits/php_wp'))
+        .pipe(gulp.dest(path_xampp));
+    }else{
+        return gulp
+        .src('./src/php_wp/**/*.+(php|png|jpg|css|js)')
+        .pipe(gulp.dest('./dits/php_wp'));
+    }
+    
+}
 
 export function templates() {
     var extName = require(path.json.package);
-    return gulp.src(path.ejs.src)
-    .pipe(plumber())
-    .pipe(frontMatter({
-      property: 'data'
-    }))
-    .pipe(ejs())
-    .pipe(wrapper({
-      layout: path.ejs.layoutDir,
-      data: {
-        name: 'ホゲのサイト',
-        layoutsDir: path.ejs.layoutDir,      
-        
-      },
-      engine: 'ejs',
-      frontMatterProp: 'data'
-    }))
-    .pipe(ejs(extName, { 'ext': '.html' }))
-    .pipe(rename({
-        extname: ".html"
-      }))
-    .pipe(gulp.dest(path.ejs.dist));
-  }
+    return gulp
+        .src(path.ejs.src)
+        .pipe(plumber())
+        .pipe(
+            frontMatter({
+                property: 'data',
+            })
+        )
+        .pipe(ejs())
+        .pipe(
+            wrapper({
+                layout: path.ejs.layoutDir,
+                data: {
+                    name: 'ホゲのサイト',
+                    layoutsDir: path.ejs.layoutDir,
+                },
+                engine: 'ejs',
+                frontMatterProp: 'data',
+            })
+        )
+        .pipe(
+            ejs(extName, {
+                ext: '.html',
+            })
+        )
+        .pipe(
+            rename({
+                extname: '.html',
+            })
+        )
+        .pipe(gulp.dest(path.ejs.dist));
+}
 
 export function watch() {
     browserSync.init({
         watch: true,
         server: {
-            baseDir: './dits'
+            baseDir: './dits',
         },
-        port: 8080
-    });      
+        port: 8080,
+    });
 
     gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel(style));
     gulp.watch('./src/assets/css/**/*.css', gulp.parallel(css));
-    gulp.watch('./src/assets/js/**/*.js', gulp.parallel(scripts));  
-    gulp.watch('./src/assets/images/**',images)
-    gulp.watch('./src/**/*.ejs',templates);
-    gulp.watch(['./dits/*.html','./src/assets/sass/**/*.scss']).on('change', browserSync.reload);
-    
+    gulp.watch('./src/assets/js/**/*.js', gulp.parallel(scripts));
+    gulp.watch('./src/assets/images/**', images);
+    gulp.watch('./src/assets/videos/**', videos);
+    gulp.watch('./src/php_wp/**', php_wp);
+    gulp.watch('./src/**/*.ejs', templates);
+    gulp.watch(['./dits/*.html', './src/assets/sass/**/*.scss']).on(
+        'change',
+        browserSync.reload
+    );
 }
 
 function post_css() {
     // console.log(sass);
     let processors = [
         sortMediaQueries({
-            sort: "mobile-first",
+            sort: 'mobile-first',
         }),
     ];
-    return gulp.src('./src/assets/scss/*.scss')
-        .pipe(sass({ outputStyle: 'expanded' })
-            .on('error', sass.logError)).pipe(autoprefixer({
-              cascade: false
-          })).pipe(postcss(processors))
-        .pipe(gulp.dest('./dits/assets/css'))
+    return gulp
+        .src('./src/assets/scss/*.scss')
+        .pipe(
+            sass({
+                outputStyle: 'expanded',
+            }).on('error', sass.logError)
+        )
+        .pipe(
+            autoprefixer({
+                cascade: false,
+            })
+        )
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dits/assets/css'));
 }
 
-exports.build = gulp.series(clean, gulp.parallel(style,css, scripts, images, templates));
+exports.build = gulp.series(
+    clean,
+    gulp.parallel(style, css, scripts, images, videos, php_wp, templates)
+);
 exports.watch = watch;
 exports.post_css = post_css;
---
+++ src/php_wp/footer.php
@@ -0,0 +1,23 @@
+<footer class="c_footer">
+    <p class="coppy">©2025 creative studio*wamhouse co.,ltd. </p>
+</footer>
+
+    <div data-gdpr="wrap" id="cookiewrap">
+      <div class="inner">
+        <p class="close"><a href="javascript:void(0)" data-gdpr="button" style="display: inline-block;">同意する</a></p>
+        <p class="txt">このウェブサイトではサイトの利便性の向上を目的にクッキーを使用します。ブラウザの設定によりクッキーの機能を変更することもできます。サイトを閲覧いただく際には、クッキーの使用に同意いただく必要があります。</p>
+      </div>      
+    </div>
+    <!-- script -->  
+    <?php wp_footer(); ?>
+
+    <!-- <script src="./assets/js/jquery-3.5.1.min.js"></script>
+    <script src="./assets/js/aos.js"></script>  
+    <script src="./assets/js/common.js"></script>  
+    <script src="./assets/js/top.js"></script> -->
+  
+ 
+  </div>
+  
+</body>
+</html>
