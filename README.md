91c3da1f5d10665192e45a6544e27e5b94e3a440
hiep011094 <75836130+hiep011094@users.noreply.github.com>
2025-09-15 19:26:04 +0700

    fix

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 1. Setting "npm i"
 2. Start "npm start"
 
+docker-compose down -d
+docker-compose up -d
+
 
 * When done, you can format the css code up the server with the command "gulp post_css"
--- gulpfile.js
+++ gulpfile.js
@@ -173,6 +173,71 @@ function templates() {
     .pipe(gulp.dest(path.ejs.dist));
 }
 
+function addUnlinkHandler02(watcher, srcDir, distDir) {
+  watcher.on('unlink', (filepath) => {
+    const filePathFromSrc = pathModule.relative(srcDir, filepath); // lấy đường dẫn tương đối so với srcDir
+    const destFilePath = pathModule.join(distDir, filePathFromSrc); // map sang distDir
+    del.sync(destFilePath);
+    console.log(`🗑️ Deleted: ${destFilePath}`);
+  });
+}
+function addUnlinkHandler(watcher, srcDir, distDirs = []) {
+  watcher.on("unlink", (filepath) => {
+    const filePathFromSrc = pathModule.relative(srcDir, filepath); // vd: "main/app.js"
+
+    distDirs.forEach(distDir => {
+      const destFilePath = pathModule.join(distDir, filePathFromSrc);
+      del.sync(destFilePath);
+      console.log(`🗑️ Deleted: ${destFilePath}`);
+    });
+  });
+}
+
+
+function addUnlinkHandler01(watcher, srcDir, distDir, options = {}) {
+  watcher.on('unlink', (filepath) => {
+    const filePathFromSrc = pathModule.relative(srcDir, filepath);
+    let destFilePath = pathModule.join(distDir, filePathFromSrc);
+
+    // Nếu là EJS thì đổi sang .html
+    if (options.extReplace) {
+      destFilePath = destFilePath.replace(/\.ejs$/, options.extReplace);
+    }
+
+    del.sync(destFilePath);
+    console.log(`🗑️ Deleted: ${destFilePath}`);
+  });
+}
+
+// function addScssUnlinkHandler(watcher, srcDir, distDir) {
+//   watcher.on("unlink", (filepath) => {
+//     const filePathFromSrc = pathModule.relative(srcDir, filepath); // ví dụ: "components/foo.scss"
+//     const baseName = filePathFromSrc.replace(/\.scss$/, "");       // "components/foo"
+
+//     const cssFile = pathModule.join(distDir, baseName + ".css");
+//     const mapFile = pathModule.join(distDir, baseName + ".css.map");
+
+//     del.sync([cssFile, mapFile]);
+//     console.log(`🗑️ Deleted: ${cssFile}, ${mapFile}`);
+//   });
+// }
+
+function addScssUnlinkHandler(watcher, srcDir, distDirs = []) {
+  watcher.on("unlink", (filepath) => {
+    const filePathFromSrc = pathModule.relative(srcDir, filepath); 
+    const baseName = filePathFromSrc.replace(/\.scss$/, ""); // bỏ đuôi .scss
+
+    distDirs.forEach(distDir => {
+      const cssFile = pathModule.join(distDir, baseName + ".css");
+      const mapFile = pathModule.join(distDir, baseName + ".css.map");
+
+      del.sync([cssFile, mapFile]);
+      console.log(`🗑️ Deleted: ${cssFile}, ${mapFile}`);
+    });
+  });
+}
+
+
 // PostCSS
 function post_css() {
   const processors = [
@@ -186,6 +251,24 @@ function post_css() {
 }
 
 // Watch task
+// function watchFiles() {
+//   browserSync.init({
+//     watch: true,
+//     server: { baseDir: './public' },
+//     port: 8080,
+//   });
+
+//   gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel(style));
+//   gulp.watch('./src/assets/vender/**', vender);
+//   gulp.watch('./src/assets/js/**/*.js', gulp.parallel(scripts));
+//   gulp.watch('./src/assets/images/**', images);
+//   gulp.watch('./src/assets/videos/**', videos);
+//   gulp.watch('./src/theme_wp/**', theme_wp);
+//   gulp.watch('./src/**/*.ejs', templates);
+//   gulp.watch(['./public/*.html', './src/assets/sass/**/*.scss']).on('change', browserSync.reload);
+
+  
+// }
 function watchFiles() {
   browserSync.init({
     watch: true,
@@ -193,15 +276,72 @@ function watchFiles() {
     port: 8080,
   });
 
-  gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel(style));
-  gulp.watch('./src/assets/vender/**', vender);
-  gulp.watch('./src/assets/js/**/*.js', gulp.parallel(scripts));
-  gulp.watch('./src/assets/images/**', images);
-  gulp.watch('./src/assets/videos/**', videos);
-  gulp.watch('./src/theme_wp/**', theme_wp);
-  gulp.watch('./src/**/*.ejs', templates);
+  // SCSS
+  // gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel(style));
+   // SCSS
+  const scssWatcher = gulp.watch('./src/assets/scss/**/*.scss', gulp.parallel(style));
+  const scssDestDirs = [
+    pathModule.resolve('./public/assets/css'),
+    pathModule.resolve(`./src/theme_wp/wp-content/themes/${rootFolder}/assets/css`)
+  ];
+  if (path_xampp) scssDestDirs.push(pathModule.resolve(`${path_xampp}/assets/css`));
+
+  addScssUnlinkHandler(
+    scssWatcher,
+    pathModule.resolve('./src/assets/scss'),
+    scssDestDirs
+  );
+  
+  // Vender
+  const venderWatcher = gulp.watch('./src/assets/vender/**', vender);
+  addUnlinkHandler(venderWatcher, pathModule.resolve('./src/assets/vender'), [
+    pathModule.resolve('./public/assets/vender'),
+    pathModule.resolve(`./src/theme_wp/wp-content/themes/${rootFolder}/assets/vender`),
+    path_xampp ? pathModule.resolve(`${path_xampp}/assets/vender`) : null
+  ].filter(Boolean));
+
+  // JS
+  const jsWatcher = gulp.watch('./src/assets/js/**/*.js', gulp.parallel(scripts));
+  addUnlinkHandler(jsWatcher, pathModule.resolve('./src/assets/js'), [
+    pathModule.resolve('./public/assets/js'),
+    pathModule.resolve(`./src/theme_wp/wp-content/themes/${rootFolder}/assets/js`),
+    path_xampp ? pathModule.resolve(`${path_xampp}/assets/js`) : null
+  ].filter(Boolean));
+
+  // Images
+  const imgWatcher = gulp.watch('./src/assets/images/**', images);
+  addUnlinkHandler(imgWatcher, pathModule.resolve('./src/assets/images'), [
+    pathModule.resolve('./public/assets/images'),
+    pathModule.resolve(`./src/theme_wp/wp-content/themes/${rootFolder}/assets/images`),
+    path_xampp ? pathModule.resolve(`${path_xampp}/assets/images`) : null
+  ].filter(Boolean));
+
+  // Videos
+  const videoWatcher = gulp.watch('./src/assets/videos/**', videos);
+  addUnlinkHandler(videoWatcher, pathModule.resolve('./src/assets/videos'), [
+    pathModule.resolve('./public/assets/videos'),
+    pathModule.resolve(`./src/theme_wp/wp-content/themes/${rootFolder}/assets/videos`),
+    path_xampp ? pathModule.resolve(`${path_xampp}/assets/videos`) : null
+  ].filter(Boolean));
+
+  // Theme WP
+  const themeWatcher = gulp.watch('./src/theme_wp/**', theme_wp);
+  addUnlinkHandler02(themeWatcher, pathModule.resolve('./src/theme_wp'), pathModule.resolve('./public/theme_wp'));
+
+  // EJS
+  // gulp.watch('./src/**/*.ejs', templates);
+  const ejsWatcher = gulp.watch('./src/**/*.ejs', templates);
+  addUnlinkHandler01(
+    ejsWatcher,
+    pathModule.resolve('./src'),
+    pathModule.resolve('./public'),
+    { extReplace: '.html' }
+  );
+
+  // Reload
   gulp.watch(['./public/*.html', './src/assets/sass/**/*.scss']).on('change', browserSync.reload);
 }
+
 
 // Export
 const build = gulp.series(
